3befcc4c39fb10a713124cd5a6c2207cbe9211ff
gorgun <bengorkem12345@gmail.com>
2023-10-15 10:31:44 +0300

    Legacy Change

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 50) { //legacy
+    if (rank > 75) { //legacy
         return 0;
     }
     if (rank > 30 && percent < 100) { //extended
