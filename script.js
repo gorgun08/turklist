f28ffdfc832c8d4f047c368063766a194a5539e4
gorgun <54141536+gorgun08@users.noreply.github.com>
2025-07-29 21:15:28 +0300

    En son...

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let counter = document.querySelector('h1');
-let count = 1;
-setInterval(()=>{
-    counter.innerText = count;
-    count++
-    
-    if (count > 3) {
+document.addEventListener('DOMContentLoaded', () => {
+    let counter = document.querySelector('h1');
+    let count = 1;
+
+    if (!counter) return;
+
+    const interval = setInterval(() => {
+        counter.innerText = count;
+        count++;
+
+        if (count > 3) {
             clearInterval(interval);
             setTimeout(() => {
-                location.replace('https://turklist.xyz');
+                location.replace('https://turklist.xyz'/);
             }, 5000);
         }
-    
-    
-},500)
+    }, 500);
+});
